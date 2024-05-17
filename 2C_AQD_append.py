8f81736db0fdff838a1c42af4b9dfbffc31fb629
JessicaMarggraf <162599382+JessicaMarggraf@users.noreply.github.com>
2024-05-17 11:09:29 +0200

    Update 2C_AQD_append.py

--- 2C_AQD_append.py
+++ 2C_AQD_append.py
@@ -2,6 +2,21 @@
 """
 Created on Fri Oct 29 16:30:55 2021
 
+Copyright (C) 2024  INRAE
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 @author: jessica.laible
 
 Prepares necessary input files for further analysis (Beam_averaged_attenuation_backscatter)
