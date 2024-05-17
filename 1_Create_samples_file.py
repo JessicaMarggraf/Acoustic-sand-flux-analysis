af9a8be2ca38688538dced62e109e363ec8e39de
JessicaMarggraf <162599382+JessicaMarggraf@users.noreply.github.com>
2024-05-17 11:08:33 +0200

    Update 1_Create_samples_file.py

--- 1_Create_samples_file.py
+++ 1_Create_samples_file.py
@@ -2,7 +2,24 @@
 """
 Created on Thu Oct 20 15:56:57 2022
 
-Crée un fichier de prélèvement en utilisant les fichiers de sortie du code ASG (Analyse des jaugeages solides)
+Copyright (C) 2024  INRAE
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Creates the "samples" file using the outputs of the sand flux analysis program 
+developed by INRAE: https://gitlab.irstea.fr/jessica.laible/analysis-solid-gauging
+
 
 @author: jlaible
 """
