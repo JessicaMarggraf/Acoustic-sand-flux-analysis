ea7234313835e62571c9ddbc6ac56c2bd6d4b85e
JessicaMarggraf <162599382+JessicaMarggraf@users.noreply.github.com>
2024-05-17 11:10:28 +0200

    Update 3_Beam_averaging_TW16A.py

--- 3_Beam_averaging_TW16A.py
+++ 3_Beam_averaging_TW16A.py
@@ -2,6 +2,21 @@
 """
 Created on Fri Oct 29 11:33:59 2021
 
+Copyright (C) 2024  INRAE
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 @author: jessica.laible
 
 Beam-averaged attenuation and backscatter
@@ -11,13 +26,6 @@ based on Ron Griffiths USGS 2021, Topping & Wright (2016)
 Input data: output data from AQD_append script (Amplitude_velocity_freq.txt and WHD_freq.txt)
 Output data: Beam_averaged backscatter, relative backscatter, FCB, attenuation due to sediment 
              (AlphaSed) and water (AlphaW)
-
-Necessary definitions: 
-    - in and outpath
-    - used frequencies (1.)
-    - Scalefactor (1.)
-    - Conditions for outliers (3.)
-    - Guess noise floor offset (4.)
 
 """
 print('****************************************************************\n'
